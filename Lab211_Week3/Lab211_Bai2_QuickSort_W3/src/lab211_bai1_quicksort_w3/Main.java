0af89de4acdb505c38a8986e6291195f609a6f41
NguyenDinhHuyDe190538 <dinhhuy8024@gmail.com>
2025-09-27 19:34:04 +0700

    Add files via upload

---
+++ Lab211_Week3/Lab211_Bai2_QuickSort_W3/src/lab211_bai1_quicksort_w3/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab211_bai1_quicksort_w3;
+
+/**
+ *
+ * @author dinhh
+ */
+public class Main {
+    public static void main(String[] args) {
+            QuickSort qs = new QuickSort();
+    int[] array = qs.InputArray();
+    
+        System.out.println("Unorder Sort array");
+        qs.showArray(array);
+       
+        
+        qs.quickSort(array, 0, array.length -1);
+        System.out.println("order sort array");
+        
+        qs.showArray(array);
+    }
+}
